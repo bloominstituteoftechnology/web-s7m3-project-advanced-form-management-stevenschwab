e851020dd6b1b37a8d224c8382bafb4c905a0349
Steve Schwab <schwabs@umich.edu>
2024-01-12 13:01:22 -0800

    editing after watching tutorial

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import * as yup from "yup";
+import * as yup from 'yup';
 import axios from 'axios';
 
 const e = { // dictionary of validation error messages.
@@ -28,37 +28,38 @@ const formSchema = yup.object().shape({
   favLanguage: yup
     .string()
     .required(e.favLanguageRequired)
+    .trim()
     .oneOf(["javascript", "rust"], e.favLanguageOptions),
   favFood: yup
     .string()
     .required(e.favFoodRequired)
+    .trim()
     .oneOf(["pizza", "spaghetti", "broccoli"], e.favFoodOptions),
   agreement: yup
     .boolean()
     .required(e.agreementRequired)
-    .isTrue(e.agreementOptions)
+    .oneOf([true], e.agreementOptions),
 })
 
-const initialValues = {
+const initialValues = () => ({
   username: '',
   favLanguage: '',
   favFood: '',
   agreement: false,
-}
-const initialFormErrors = {
+})
+const initialFormErrors = () => ({
   username: '',
   favLanguage: '',
   favFood: '',
   agreement: '',
-}
-const initialMessage = '';
+})
 const initialDisabled = true;
 
 export default function App() {
-  const [values, setValues] = useState(initialValues);
-  const [formErrors, setFormErrors] = useState(initialFormErrors);
-  const [successMsg, setSuccessMsg] = useState(initialMessage);
-  const [failureMsg, setFailureMsg] = useState(initialMessage);
+  const [values, setValues] = useState(initialValues());
+  const [formErrors, setFormErrors] = useState(initialFormErrors());
+  const [successMsg, setSuccessMsg] = useState();
+  const [failureMsg, setFailureMsg] = useState();
   const [isDisabled, setIsDisabled] = useState(initialDisabled);
 
   useEffect(() => {
@@ -68,13 +69,13 @@ export default function App() {
   const validate = (name, value) => {
     yup.reach(formSchema, name)
       .validate(value)
-      .then(() => setFormErrors({ ...formErrors, [name]: ""}))
+      .then(() => setFormErrors({ ...formErrors, [name]: "" }))
       .catch(err => setFormErrors({ ...formErrors, [name]: err.errors[0] }))
   }
 
   const onChange = evt => {
     let { type, name, checked, value } = evt.target;
-    if (type === 'checkbox') value = checked;
+    value = type == 'checkbox' ? checked : value
     validate(name, value);
     setValues({ ...values, [name]: value });
   }
@@ -82,62 +83,64 @@ export default function App() {
   const onSubmit = evt => {
     evt.preventDefault();
     setIsDisabled(initialDisabled);
-    axios
-      .post("https://webapis.bloomtechdev.com/registration", values)
+    axios.post("https://webapis.bloomtechdev.com/registration", values)
       .then(res => {
+        setValues(initialValues())
         setSuccessMsg(res.data.message);
+        setFailureMsg();
       })
       .catch(err => {
-        setFailureMsg(err.message);
+        setFailureMsg(err.response.data.message);
+        setSuccessMsg()
       })
-      .finally(() => setValues(initialValues))
+      .finally(() => setValues(initialValues()))
   }
 
   return (
     <div>
       <h2>Create an Account</h2>
       <form onSubmit={onSubmit}>
-        {successMsg && <h4 className="success">{successMsg}</h4>}
-        {failureMsg && <h4 className="error">{failureMsg}</h4>}
+        { successMsg && <h4 className="success">{successMsg}</h4> }
+        { failureMsg && <h4 className="error">{failureMsg}</h4> }
 
         <div className="inputGroup">
           <label htmlFor="username">Username:</label>
-          <input id="username" name="username" type="text" placeholder="Type Username" onChange={onChange}/>
-          {formErrors.username && <div className="validation">{formErrors.username}</div>}
+          <input value={values.username} onChange={onChange} id="username" name="username" type="text" placeholder="Type Username"/>
+          { formErrors.username && <div className="validation">{formErrors.username}</div> }
         </div>
 
         <div className="inputGroup">
           <fieldset>
             <legend>Favorite Language:</legend>
             <label>
-              <input type="radio" name="favLanguage" value="javascript" onChange={onChange}/>
+              <input checked={values.favLanguage == 'javascript'} onChange={onChange} type="radio" name="favLanguage" value="javascript"/>
               JavaScript
             </label>
             <label>
-              <input type="radio" name="favLanguage" value="rust" onChange={onChange}/>
+              <input checked={values.favLanguage == 'rust'} onChange={onChange} type="radio" name="favLanguage" value="rust"/>
               Rust
             </label>
           </fieldset>
-          {formErrors.favLanguage && <div className="validation">{formErrors.favLanguage}</div>}
+          { formErrors.favLanguage && <div className="validation">{formErrors.favLanguage}</div> }
         </div>
 
         <div className="inputGroup">
           <label htmlFor="favFood">Favorite Food:</label>
-          <select id="favFood" name="favFood" onChange={onChange}>
+          <select value={values.favFood} onChange={onChange} id="favFood" name="favFood">
             <option value="">-- Select Favorite Food --</option>
             <option value="pizza">Pizza</option>
             <option value="spaghetti">Spaghetti</option>
             <option value="broccoli">Broccoli</option>
           </select>
-          {formErrors.favFood && <div className="validation">{formErrors.favFood}</div>}
+          { formErrors.favFood && <div className="validation">{formErrors.favFood}</div> }
         </div>
 
         <div className="inputGroup">
           <label>
-            <input id="agreement" type="checkbox" name="agreement" onChange={onChange}/>
+            <input checked={values.agreement} onChange={onChange} id="agreement" type="checkbox" name="agreement"/>
             Agree to our terms
           </label>
-          {formErrors.agreement && <div className="validation">{formErrors.agreement}</div>}
+          { formErrors.agreement && <div className="validation">{formErrors.agreement}</div> }
         </div>
 
         <div>
